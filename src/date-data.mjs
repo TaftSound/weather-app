ea58a70515a13c2e233f503130dcb907ea11467d
TaftSound <mattmurphytattoos@gmail.com>
2022-10-17 08:47:13 -0700

    Fix timezone calculation

--- src/date-data.mjs
+++ src/date-data.mjs
@@ -1,15 +1,19 @@
 import format from 'date-fns/format/index.js'
 import add from 'date-fns/add/index.js'
 
+const localeDate = new Date()
+const timeZoneOffset = localeDate.getTimezoneOffset() * 60 * 1000
+const utcDate = new Date(localeDate.getTime() + timeZoneOffset)
+
 let date = ''
 let formattedDate = ''
 
 export function getNextDays () {
-  const dayTwo = format(add(date, { days: 1 }), 'E')
-  const dayThree = format(add(date, { days: 2 }), 'E')
-  const dayFour = format(add(date, { days: 3 }), 'E')
-  const dayFive = format(add(date, { days: 4 }), 'E')
-  const daySix = format(add(date, { days: 5 }), 'E')
+  const dayTwo = format(date, 'E')
+  const dayThree = format(add(date, { days: 1 }), 'E')
+  const dayFour = format(add(date, { days: 2 }), 'E')
+  const dayFive = format(add(date, { days: 3 }), 'E')
+  const daySix = format(add(date, { days: 4 }), 'E')
   return ([dayTwo, dayThree, dayFour, dayFive, daySix])
 }
 
@@ -19,15 +23,10 @@ export function getCurrentDate () {
 }
 
 export function setLocaleDate (dataObject) {
-  const convertedDate = (dataObject.localeDate) * 1000
-  date = new Date(convertedDate)
+  const timeZoneShift = (dataObject.timeZone) * 1000
+  date = new Date(utcDate.getTime() + timeZoneShift)
 }
 
 // following for proper time conversion
 
-// const testDate = new Date()
-// const timeZoneOffset = testDate.getTimezoneOffset() * 60 * 1000
-// console.log(testDate)
-// console.log(timeZoneOffset)
-// console.log(new Date(testDate.getTime() + timeZoneOffset))
 // this is time converted to UTC, need to then add timezone for place searched
--- src/forecast-data.mjs
+++ src/forecast-data.mjs
@@ -1,91 +1,73 @@
-import format from 'date-fns/format/index.js'
 
-let currentDate
 let timeZone
 
 async function getCurrentWeather (lat, lon) {
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=c48bcffd61ca0ded777af3c42746942e`, { mode: 'cors' })
     const currentWeatherObject = await response.json()
-    return processCurrentWeatherData(currentWeatherObject)
+    return parseCurrentWeatherData(currentWeatherObject)
   } catch (error) {
     console.error('error: ', error)
     alert(error)
   }
 }
-function processCurrentWeatherData (dataObject) {
+function parseCurrentWeatherData (dataObject) {
   const temp = Math.round(dataObject.main.temp)
   const feelsLike = Math.round(dataObject.main.feels_like)
   const humidity = Math.round(dataObject.main.humidity)
   const wind = Math.round(dataObject.wind.speed)
   const description = dataObject.weather[0].description
   const iconUrl = createIconUrl(dataObject.weather[0].icon)
-  const localeDate = dataObject.dt
+  // const localeDate = dataObject.dt
   timeZone = dataObject.timezone
-  currentDate = normalizeDate(localeDate)
-  console.log(currentDate)
-  return { temp, feelsLike, humidity, wind, description, iconUrl, localeDate }
+  return { temp, feelsLike, humidity, wind, description, iconUrl, timeZone }
 }
-// function processUpcomingWeatherData (dataObject) {
-//   let dateKey = currentDate
-//   let dayNumber = 0
-//   const fiveDayData = {}
-//   const twentyFourHourData = []
-//   fiveDayData[dayNumber] = []
-//   for (let i = 0; i < 40; i++) {
-//     const currentReport = dataObject.list[i]
-//     const currentReportDate = normalizeDate(currentReport.dt)
-//     if (i < 8) {
-//       const processedReport = interpretOneDayData(currentReport)
-//       twentyFourHourData.push(processedReport)
-//     }
-//     if (currentReportDate === currentDate) { continue }
-//     if (currentReportDate !== dateKey) {
-//       dateKey = currentReportDate
-//       dayNumber++
-//       fiveDayData[dayNumber] = []
-//     }
-//     fiveDayData[dayNumber].push(currentReport)
-//   }
-//   console.log(twentyFourHourData, fiveDayData)
-//   return { twentyFourHourData, fiveDayData }
-// }
-function processFiveDayData (dataObject) {
-  let dateKey = currentDate
+async function getWeatherForecast (lat, lon) {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=c48bcffd61ca0ded777af3c42746942e`, { mode: 'cors' })
+    const weatherForecastObject = await response.json()
+    const fiveDayData = parseWeatherForecastData(weatherForecastObject)
+    return { fiveDayData }
+  } catch (error) {
+    console.error('error: ', error)
+    alert(error)
+  }
+}
+function parseWeatherForecastData (dataObject) {
+  let dateKey = ''
   let dayNumber = -1
+  const days = ['dayOne', 'dayTwo', 'dayThree', 'dayFour', 'dayFive', 'daySix']
   const fiveDayData = {}
   for (let i = 0; i < 40; i++) {
     const currentReport = dataObject.list[i]
     const currentReportDate = normalizeDate(currentReport.dt)
-    if (currentReportDate === currentDate) { continue }
     if (currentReportDate !== dateKey) {
       dateKey = currentReportDate
       dayNumber++
-      fiveDayData[dayNumber] = []
+      fiveDayData[days[dayNumber]] = []
     }
-    fiveDayData[dayNumber].push(currentReport)
+    fiveDayData[days[dayNumber]].push(currentReport)
   }
-  console.log(fiveDayData)
+  interpretFiveDayData(fiveDayData)
   return fiveDayData
-}
-// interpretFiveDayData (dataObject) {
 
-// }
-function processOneDayData (dataObject) {
-  const oneDayData = []
-  for (let i = 0; i < 8; i++) {
-    const currentReport = dataObject.list[i]
-    const processedReport = interpretOneDayData(currentReport)
-    oneDayData.push(processedReport)
+  function interpretFiveDayData (dataObject) {
+    for (const day in dataObject) {
+      const currentDayArray = dataObject[day]
+      for (let j = 0; j < currentDayArray.length; j++) {
+        const currentReport = currentDayArray[j]
+        const processedReport = interpretOneDayData(currentReport)
+        currentDayArray[j] = processedReport
+      }
+    }
   }
-  console.log(oneDayData)
-  return oneDayData
-}
-function interpretOneDayData (dataObject) {
-  const temp = dataObject.main.temp
-  const humidity = dataObject.main.humidity
-  const wind = dataObject.wind.speed
-  return { temp, humidity, wind }
+  function interpretOneDayData (dataObject) {
+    const temp = dataObject.main.temp
+    const humidity = dataObject.main.humidity
+    const wind = dataObject.wind.speed
+    const precipitation = dataObject.pop
+    return { temp, humidity, wind, precipitation }
+  }
 }
 function createIconUrl (iconKey) {
   return `http://openweathermap.org/img/wn/${iconKey}@4x.png`
@@ -94,19 +76,6 @@ function normalizeDate (dateValue) {
   let localeTime = new Date((dateValue + timeZone) * 1000).toUTCString()
   localeTime = localeTime.split(',')
   return localeTime[0]
-}
-
-async function getWeatherForecast (lat, lon) {
-  try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=c48bcffd61ca0ded777af3c42746942e`, { mode: 'cors' })
-    const weatherForecastObject = await response.json()
-    const oneDayData = processOneDayData(weatherForecastObject)
-    const fiveDayData = processFiveDayData(weatherForecastObject)
-    return { oneDayData, fiveDayData }
-  } catch (error) {
-    console.error('error: ', error)
-    alert(error)
-  }
 }
 
 export const retrieveForecastData = async (lat, lon) => {
